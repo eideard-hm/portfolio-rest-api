d2a4b87a525ec76530d42bf240b7514a7b9238b7
eideard-hm <edierhernandezmo@gmail.com>
2023-01-09 18:26:15 -0500

    Retrive port from env

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.JSON(fiber.Map{
+			"data": "Hello Word",
+		})
 	})
 
-	app.Listen(":3000")
+	app.Listen(fmt.Sprintf(":%s", port))
 }
